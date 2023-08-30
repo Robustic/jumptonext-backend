714cef9ceb96aab0625aad503e5ef93cdbffc1c8
Robustic <juhas.malinen@gmail.com>
2023-08-30 15:52:26 +0300

    All stops or single stop can be queried from digitransit api.

--- schema.js
+++ schema.js
@@ -8,9 +8,43 @@ const typeDefs = `
     type Token {
         value: String!
     }
+      
+    type Stop {
+        gtfsId: String!
+        name: String!
+        lat: Float!
+        lon: Float!
+        code: String
+        zoneId: String
+        vehicleType: Int!
+        stoptimesWithoutPatterns: [StoptimesWithoutPatterns!]!
+    }
+
+    type Trip {
+        id: String!
+        routeShortName: String!
+    }
+
+    type StoptimesWithoutPatterns {
+        scheduledArrival: Int!
+        realtimeArrival: Int!
+        arrivalDelay: Int!
+        scheduledDeparture: Int!
+        realtimeDeparture: Int!
+        departureDelay: Int!
+        realtime: Boolean!
+        realtimeState: String!
+        serviceDay: Int!
+        headsign: String!
+        trip: Trip
+    }
 
     type Query {
         me: User
+        stops: [Stop!]!
+        stop(
+            id: String!
+        ): Stop
     }
 
     type Mutation {
